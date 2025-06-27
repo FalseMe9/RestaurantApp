f3520b4507281b8b0fe052875352f7c09a4e67c2
Billie <150131870+FalseMe9@users.noreply.github.com>
2025-06-27 16:11:19 +0700

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Billie Hartanto on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
